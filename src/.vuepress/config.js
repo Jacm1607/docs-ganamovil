5b949582f42e82c19a3188a656a04b4706b36eaf
Jose Chavarria <jose.chavarria@bg.com.bo>
2022-09-30 15:25:49 -0400

    v1.0

---
+++ src/.vuepress/config.js
@@ -0,0 +1,58 @@
+const { description } = require('../../package')
+
+module.exports = {
+  title: 'GanaMóvil v3',
+  description: 'Documento de los iconos en el nuevo ganamovil',
+  dest:'docs',
+  base:'/docs-rn/',
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    editLinks: false,
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: 'Guia',
+        link: '/',
+      },
+      {
+        text: 'Config',
+        link: '/config/'
+      },
+      {
+        text: 'GanaMóvil',
+        link: 'https://v1.vuepress.vuejs.org'
+      }
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          title: 'Guide',
+          collapsable: false,
+          children: [
+            '',
+            'using-vue',
+          ]
+        }
+      ],
+    }
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: [
+    '@vuepress/plugin-back-to-top',
+    '@vuepress/plugin-medium-zoom',
+  ]
+}
